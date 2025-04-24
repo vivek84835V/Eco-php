571305a7179e0987f362b4136ed8ac17826b20f8
vivek shimpi <your.vivekshimpi146@gmail.com.com>
2025-04-24 14:02:10 +0530

    changes model path

--- public/admin/dashboard.php
+++ public/admin/dashboard.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['admin'])) {
 }
 
 require_once '../../config/config.php';
-require_once '../app/Models/Product.php';
+require_once '../../app/Models/product.php';
 $productModel = new Product($pdo);
 $products = $productModel->getAll();
 ?>
